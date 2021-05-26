919b3da137e2f2996f223b8539e52ce098fd11b8
SomnathBhandari <bhandarisomnath1998@gmail.com>
2021-05-26 14:08:59 +0530

    car committed successfully

---
+++ src/main/java/com/som/beans/Car.java
@@ -0,0 +1,18 @@
+package com.som.beans;
+
+public class Car {
+
+	public Car() {
+		System.out.println("Car.Car()");
+	}
+
+	@Override
+	public String toString() {
+		return "Car []";
+	}
+	
+	
+	
+	
+
+}
---
+++ src/main/java/com/som/test/CarTest.java
@@ -0,0 +1,15 @@
+package com.som.test;
+
+import com.som.beans.Car;
+
+public class CarTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Car c=new Car();
+		System.out.println(c);
+
+	}
+
+}
